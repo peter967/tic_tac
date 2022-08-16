b7ef1395997ab16a39a50211ac9a46bfc51aa7e5
peter967 <pnapil96@gmail.com>
2022-08-16 14:50:11 +0200

    first commit

---
+++ lib/game_logic.dart
@@ -0,0 +1,92 @@
+import 'dart:math';
+
+class Player {
+  static const x = 'X';
+  static const o = 'O';
+  static const empty = '';
+  static List<int> playerX = [];
+  static List<int> playerO = [];
+}
+
+extension ContainsAll on List {
+  bool containsAll(int x, int y, [z]) {
+    if (z == null) {
+      return contains(x) && contains(y);
+    }
+    return contains(x) && contains(y) && contains(z);
+  }
+}
+
+class Game {
+  void playGame(int index, String activePlayer) {
+    if (activePlayer == 'X') {
+      Player.playerX.add(index);
+    } else {
+      Player.playerO.add(index);
+    }
+  }
+
+  checkWinner() {
+    String winner = '';
+
+    if (Player.playerX.containsAll(0, 1, 2) ||
+        Player.playerX.containsAll(3, 4, 5) ||
+        Player.playerX.containsAll(6, 7, 8) ||
+        Player.playerX.containsAll(0, 4, 8) ||
+        Player.playerX.containsAll(2, 4, 6) ||
+        Player.playerX.containsAll(0, 3, 6) ||
+        Player.playerX.containsAll(1, 4, 7) ||
+        Player.playerX.containsAll(2, 5, 8) ||
+        Player.playerX.containsAll(2, 4, 6)) {
+      winner = 'X';
+    } else if (Player.playerO.containsAll(0, 1, 2) ||
+        Player.playerO.containsAll(3, 4, 5) ||
+        Player.playerO.containsAll(6, 7, 8) ||
+        Player.playerO.containsAll(0, 4, 8) ||
+        Player.playerO.containsAll(2, 4, 6) ||
+        Player.playerO.containsAll(0, 3, 6) ||
+        Player.playerO.containsAll(1, 4, 7) ||
+        Player.playerO.containsAll(2, 5, 8) ||
+        Player.playerO.containsAll(2, 4, 6)) {
+      winner = 'O';
+    } else {
+      winner = '';
+    }
+
+    return winner;
+  }
+
+  Future<void> autoPlay(activePlayer) async {
+    int index;
+    List<int> emptyCells = [];
+    for (var i = 0; i < 9; i++) {
+      if (!(Player.playerX.contains(i) || Player.playerO.contains(i))) {
+        emptyCells.add(i);
+      }
+    }
+    if (Player.playerX.containsAll(0, 1) && emptyCells.contains(2)) {
+      index = 2;
+    } else if (Player.playerX.containsAll(3, 4) && emptyCells.contains(5)) {
+      index = 5;
+    } else if (Player.playerX.containsAll(6, 7) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(0, 4) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(2, 4) && emptyCells.contains(6)) {
+      index = 6;
+    } else if (Player.playerX.containsAll(0, 3) && emptyCells.contains(6)) {
+      index = 6;
+    } else if (Player.playerX.containsAll(1, 4) && emptyCells.contains(7)) {
+      index = 7;
+    } else if (Player.playerX.containsAll(2, 5) && emptyCells.contains(8)) {
+      index = 8;
+    } else if (Player.playerX.containsAll(2, 4) && emptyCells.contains(6)) {
+      index = 6;
+    } else {
+      Random random = Random();
+      int randomIndex = random.nextInt(emptyCells.length);
+      index = emptyCells[randomIndex];
+    }
+    playGame(index, activePlayer);
+  }
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac/game_logic.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String activePlayer = 'X';
+  bool gameOver = false;
+  int turn = 0;
+  String result = '';
+  Game game = Game();
+  bool isSwitched = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: MediaQuery.of(context).orientation == Orientation.portrait
+            ? Column(
+                children: [
+                  ...firstBlock(),
+                  _expanded(context),
+                  ...lastBlock(),
+                ],
+              )
+            : Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ...firstBlock(),
+                        ...lastBlock(),
+                      ],
+                    ),
+                  ),
+                  _expanded(context),
+                ],
+              ),
+      ),
+    );
+  }
+
+  List<Widget> firstBlock() {
+    return [
+      SwitchListTile.adaptive(
+        title: const Text(
+          'turn on/of two player',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        value: isSwitched,
+        onChanged: (newValue) => setState(() => isSwitched = newValue),
+      ),
+      Text(
+        " it's $activePlayer turn".toUpperCase(),
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 50,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ];
+  }
+
+  List<Widget> lastBlock() {
+    return [
+      Text(
+        result,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 40,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: ElevatedButton.icon(
+          onPressed: () => setState(() {
+            Player.playerX = [];
+            Player.playerO = [];
+            gameOver = false;
+            turn = 0;
+            result = '';
+          }),
+          icon: const Icon(Icons.replay),
+          label: const Text('Repeat the Game'),
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all(Theme.of(context).splashColor),
+          ),
+        ),
+      ),
+    ];
+  }
+
+  Expanded _expanded(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        padding: const EdgeInsets.all(16),
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
+        childAspectRatio: 1.0,
+        crossAxisCount: 3,
+        children: List.generate(
+          9,
+          (index) => InkWell(
+            borderRadius: BorderRadius.circular(16),
+            onTap: gameOver ? null : () => _onTab(index),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).shadowColor,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Center(
+                child: Text(
+                  Player.playerX.contains(index)
+                      ? 'X'
+                      : Player.playerO.contains(index)
+                          ? 'O'
+                          : '',
+                  style: TextStyle(
+                    color: Player.playerX.contains(index)
+                        ? Colors.blue
+                        : Colors.red,
+                    fontSize: 50,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _onTab(int index) async {
+    if ((Player.playerX.isEmpty || !Player.playerX.contains(index)) &&
+        (Player.playerO.isEmpty || !Player.playerO.contains(index))) {
+      game.playGame(index, activePlayer);
+      updateState();
+      if (!isSwitched && !gameOver && turn != 9) {
+        await game.autoPlay(activePlayer);
+        updateState();
+      }
+    }
+  }
+
+  void updateState() {
+    setState(() {
+      activePlayer = activePlayer == 'X' ? 'O' : 'X';
+      turn++;
+      String winnerPlayer = game.checkWinner();
+      if (winnerPlayer != '') {
+        gameOver = true;
+        result = '$winnerPlayer is the winner';
+      } else if (!gameOver && turn == 9) {
+        {
+          result = "it's Draw!";
+        }
+      }
+    });
+  }
+}
